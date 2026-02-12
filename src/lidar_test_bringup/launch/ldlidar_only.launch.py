2bf1165446e42db1ca3ac112545871e64e2c57ac
Poly-Zeta <vpolyzeta+github@gmail.com>
2026-02-13 06:12:17 +0900

    コンフィグの変更

--- src/lidar_test_bringup/launch/ldlidar_only.launch.py
+++ src/lidar_test_bringup/launch/ldlidar_only.launch.py
@@ -1,20 +1,29 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    port_name = LaunchConfiguration('port_name')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'port_name',
+            default_value='/dev/ldlidar',
+            description='Serial device path for LDLiDAR'
+        ),
+
         Node(
             package='ldlidar_ros2',
             executable='ldlidar_ros2_node',
             name='ldlidar_publisher_ld06',
+            output='screen',
             parameters=[{
                 'product_name': 'LDLiDAR_LD06',
                 'laser_scan_topic_name': 'scan',
                 'point_cloud_2d_topic_name': 'pointcloud2d',
                 'frame_id': 'laser',
-                'port_name': '/dev/ttyAMA1',
+                'port_name': port_name,
                 'serial_baudrate': 230400,
                 'laser_scan_dir': True,
                 'enable_angle_crop_func': False,
@@ -25,5 +34,3 @@ def generate_launch_description():
             }]
         )
     ])
-
-
